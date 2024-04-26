9fd52a437cd9c7ae8e9f64b19807310d082b355c
youmin1017 <xx4016481@gmail.com>
2024-04-26 14:41:10 +0800

    update nvim cmp mapping

--- dot_config/nvim/lua/plugins/override.lua
+++ dot_config/nvim/lua/plugins/override.lua
@@ -1,111 +1,110 @@
 ---@type NvPluginSpec[]
 local plugins = {
-  -- overrde plugin configs
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "vim",
-        "lua",
-        "c",
-      },
-      indent = {
-        enable = true,
-        disable = {
-          "python",
-        },
-      },
-    },
-  },
+	-- overrde plugin configs
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"vim",
+				"lua",
+				"c",
+			},
+			indent = {
+				enable = true,
+				disable = {
+					"python",
+				},
+			},
+		},
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- lua stuff
-        "lua-language-server",
-        "stylua",
-        "json-lsp",
-        "taplo",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- lua stuff
+				"lua-language-server",
+				"stylua",
+				"json-lsp",
+				"taplo",
+			},
+		},
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      on_attach = require "configs.nvim-tree",
-      trash = {
-        cmd = "trash -F",
-      },
-      experimental = {
-        git = {
-          async = true,
-        },
-      },
-      git = {
-        enable = true,
-      },
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-          },
-          glyphs = {
-            git = {
-              untracked = "", -- "󰧟",
-            },
-          },
-        },
-      },
-    },
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			on_attach = require("configs.nvim-tree"),
+			trash = {
+				cmd = "trash -F",
+			},
+			experimental = {
+				git = {
+					async = true,
+				},
+			},
+			git = {
+				enable = true,
+			},
+			renderer = {
+				highlight_git = true,
+				icons = {
+					show = {
+						git = true,
+					},
+					glyphs = {
+						git = {
+							untracked = "", -- "󰧟",
+						},
+					},
+				},
+			},
+		},
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = function()
-      local conf = require "nvchad.configs.telescope"
-      conf.defaults.mappings.i = {
-        ["<C-j>"] = require("telescope.actions").move_selection_next,
-        ["<C-k>"] = require("telescope.actions").move_selection_previous,
-      }
-      return conf
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = function()
+			local conf = require("nvchad.configs.telescope")
+			conf.defaults.mappings.i = {
+				["<C-j>"] = require("telescope.actions").move_selection_next,
+				["<C-k>"] = require("telescope.actions").move_selection_previous,
+			}
+			return conf
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local conf = require "nvchad.configs.cmp"
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local conf = require("nvchad.configs.cmp")
+			local cmp = require("cmp")
 
-      conf.mapping = {
-        ["<C-k>"] = require("cmp").mapping.select_prev_item(),
-        ["<C-j>"] = require("cmp").mapping.select_next_item(),
-        ["<C-s>"] = require("cmp").mapping.complete(),
-        ["<C-Space>"] = {},
-      }
+			conf.mapping["<C-k>"] = cmp.mapping.select_prev_item()
+			conf.mapping["<C-j>"] = cmp.mapping.select_next_item()
+			conf.mapping["<C-s>"] = cmp.mapping.complete()
+			conf.mapping["<C-Space>"] = nil
 
-      table.insert(conf.sources, { name = "crates" })
-      table.insert(conf.sources, { name = "vimtex" })
+			table.insert(conf.sources, { name = "crates" })
+			table.insert(conf.sources, { name = "vimtex" })
 
-      return conf
-    end,
-  },
+			return conf
+		end,
+	},
 
-  {
-    "folke/which-key.nvim",
-    opts = {
-      plugins = {
-        presets = {
-          operators = false,
-        },
-      },
-    },
-  },
+	{
+		"folke/which-key.nvim",
+		opts = {
+			plugins = {
+				presets = {
+					operators = false,
+				},
+			},
+		},
+	},
 }
 
 return plugins
