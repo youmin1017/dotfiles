fcd37a793b401c9862ca83f415221ef6d735d486
youmin1017 <xx4016481@gmail.com>
2024-04-26 13:53:03 +0800

    update nvim config

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -44,13 +44,13 @@ return {
 
   -- overrde plugin configs
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
   --
   -- {
   -- 	"williamboman/mason.nvim",
--- dot_config/nvim/lua/plugins/override.lua
+++ dot_config/nvim/lua/plugins/override.lua
@@ -1,48 +1,110 @@
-local overrides = require "configs.overrides"
-
 ---@type NvPluginSpec[]
 local plugins = {
   -- overrde plugin configs
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
+    opts = {
+      autotag = {
+        enable = true,
+      },
+      ensure_installed = {
+        "vim",
+        "lua",
+        "c",
+      },
+      indent = {
+        enable = true,
+        disable = {
+          "python",
+        },
+      },
+    },
   },
 
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason,
+    opts = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+        "json-lsp",
+        "taplo",
+      },
+    },
   },
 
   {
     "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
+    opts = {
+      on_attach = require "configs.nvim-tree",
+      trash = {
+        cmd = "trash -F",
+      },
+      experimental = {
+        git = {
+          async = true,
+        },
+      },
+      git = {
+        enable = true,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+          },
+          glyphs = {
+            git = {
+              untracked = "", -- "󰧟",
+            },
+          },
+        },
+      },
+    },
   },
 
   {
     "nvim-telescope/telescope.nvim",
-    opts = overrides.telescope,
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
     opts = function()
-      local merge_tb = vim.tbl_deep_extend
-      local M = require "nvchad.configs.cmp"
-      M = merge_tb("force", M, overrides.cmp)
-      table.insert(M.sources, { name = "crates" })
-      table.insert(M.sources, { name = "vimtex" })
-      return M
+      local conf = require "nvchad.configs.telescope"
+      conf.defaults.mappings.i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+      }
+      return conf
     end,
   },
 
   {
     "hrsh7th/nvim-cmp",
-    opts = overrides.cmp,
+    opts = function()
+      local conf = require "nvchad.configs.cmp"
+
+      conf.mapping = {
+        ["<C-k>"] = require("cmp").mapping.select_prev_item(),
+        ["<C-j>"] = require("cmp").mapping.select_next_item(),
+        ["<C-s>"] = require("cmp").mapping.complete(),
+        ["<C-Space>"] = {},
+      }
+
+      table.insert(conf.sources, { name = "crates" })
+      table.insert(conf.sources, { name = "vimtex" })
+
+      return conf
+    end,
   },
 
   {
     "folke/which-key.nvim",
-    opts = overrides.whichkey,
+    opts = {
+      plugins = {
+        presets = {
+          operators = false,
+        },
+      },
+    },
   },
 }
 
