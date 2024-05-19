585b5e5100b1b208ef27db1d052f049101a4c2aa
youmin1017 <xx4016481@gmail.com>
2024-05-20 00:56:55 +0800

    update config

---
+++ dot_config/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,23 @@
+local bufnr = vim.api.nvim_get_current_buf()
+local function desc(description)
+  return { noremap = true, silent = true, buffer = bufnr, desc = description }
+end
+
+vim.keymap.set("v", "K", function()
+  vim.cmd.RustLsp { "hover", "range" }
+end, desc "Rust hover range")
+vim.keymap.set("n", "<leader>rtt", function()
+  vim.cmd.RustLsp "testables"
+end, desc "Rust [t]es[t]ables")
+vim.keymap.set("n", "<leader>rtl", function()
+  vim.cmd.RustLsp { "testables", bang = true }
+end, desc "Rust run [t]estables [l]ast")
+vim.keymap.set("n", "<leader>rk", function()
+  vim.cmd.RustLsp { "moveItem", "up" }
+end, desc "Rust move item up [k]")
+vim.keymap.set("n", "<leader>rj", function()
+  vim.cmd.RustLsp { "moveItem", "down" }
+end, desc "Rust move item down [j]")
+-- vim.keymap.set("n", "<leader>ca", function()
+--   vim.cmd.RustLsp "codeAction"
+-- end, desc "Rust [c]ode [a]ctions")
